88a72d16ce2e681ecf4c87653c0e4a7bbbb06b36
lorgik <sib.lorgik@gmail.com>
2022-03-22 16:43:49 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className='app'>
+            <div className='header'>
+                <div>
+                    <h1>Todo list</h1>
+                </div>
+            </div>
+            <div className='main'>
+                MAIN
+            </div>
+        </div>
+    );
+}
+
+export default App;
